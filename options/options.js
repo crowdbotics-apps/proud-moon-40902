378542cb4b0ba834238a751065c840305f0ca6b2
Crowdbotics <team@crowdbotics.com>
2023-05-22 21:05:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "proud-moon-40902",
+  url: "https://proud-moon-40902.botics.co",
+  api: "https://proud-moon-40902.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
